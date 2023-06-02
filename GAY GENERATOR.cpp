c85ec518fa65968a62fe5ad121c3df6964446c45
Chrysolite <Narutohype237@gmail.com>
2023-06-01 22:26:27 -0500

    MESSAGE

---
+++ GAY GENERATOR.cpp
@@ -0,0 +1,35 @@
+// GAY GENERATOR.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main() {
+	
+	// Providing a seed value
+	srand((unsigned)time(NULL));
+
+	// Loop to get 1 random number
+	for (int i = 1; i <= 1; i++) {
+
+		// Retrieve a random number between 1 and 100
+		// Offset = 1
+		// Range = 101
+		int random = 1 + (rand() % 101);
+
+		// Print the random number
+		cout << random << endl;
+
+		return 1;
+		
+		int rand = 0;
+		std::cout << "You are " << rand << "percent gay\n";
+
+
+
+	}
+
+	
